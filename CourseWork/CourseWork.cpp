5aece0f472e0ad3a51fc404dfd783e7ef4f2d7fa
Kirill <kirrilf@mail.ru>
2019-05-31 00:46:13 +0300

    two commit

--- CourseWork/CourseWork.cpp
+++ CourseWork/CourseWork.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <conio.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
 
 
 
@@ -11,7 +17,8 @@ using namespace std;
 
 struct Product {
 	Product *next;
-
+	
+	
 	int productId;
 	int deadline;
 	int amount;
@@ -30,7 +37,7 @@ struct Equipment {
 
 	int equipmentID;
 	int speedPerHour;
-	int time = 0;
+	int time = 1552867200;
 
 
 
@@ -51,6 +58,15 @@ struct plan {
 plan *firstElementPlan;
 plan *lastElementPlan;
 
+struct jsonBlock {
+	char *A = new char[100];
+	char *B = new char[100];
+	char *C = new char[100];
+	char *D = new char[100];
+	jsonBlock *next;
+};
+jsonBlock *firstElementJsonBlock;
+jsonBlock *lastElementJsonBlock;
 
 
 Product * mergeListsProduct(Product * a, Product * b) {
@@ -165,18 +181,24 @@ Equipment * mergeSortEquipment(Equipment * c) {
 }
 
 
-
 void Cof() {
 	Product *temp = new Product;
 	temp = firstElementProduct;
 
 	while (temp) {
 		int cof;
+		if (!temp) {
+
+		}
 		cof = temp->deadline / temp->amount;
+		//cof = 1;
 
 		temp->cof = cof;
-		
+		if (temp->next == NULL) {
+			return;
+		}
 		temp = temp->next;
+		
 	}
 }
 
@@ -203,15 +225,37 @@ void AddElemenToListEquipment(int speedPerHour, int equipmentID)
 
 }
 
+void AddElemenToListJsonEquipment(char A[], char B[], char C[], char D[], int lenghtA, int lenghtB, int lenghtC, int lenghtD ){
+	
+	int speedPerHour = 0;
+	int equipmentID = 0;
+	for (int i = 0; i < lenghtD; i++) {
+		speedPerHour = 10 * speedPerHour + (D[i] - '0');
+	}
+	for (int i = 0; i < lenghtB; i++) {
+		equipmentID = 10 * equipmentID + (B[i] - '0');
+	}
 
-void AddElemenToListProduct(int deadline, int amount, int productId, int equipmentClass[])
+	/*cout << speedPerHour << " " << equipmentID << endl;
+	system("pause");*/
+
+	AddElemenToListEquipment(speedPerHour, equipmentID);
+
+}
+
+
+
+
+void AddElemenToListProduct(int deadline, int amount, int IdProd)
 {
 	Product *Element = new Product;
 	Product *temp = new Product;
 	Element->amount = amount;
 	Element->deadline = deadline;
-	Element->productId = productId;
-	Element->equipmentClass = equipmentClass;
+	Element->cof= deadline / amount;
+	Element->productId = IdProd;
+	//Element->productId = productId;
+	//Element->equipmentClass = equipmentClass;
 
 	if (firstElementProduct == NULL && firstElementProduct == lastElementProduct) {
 		firstElementProduct = Element;
@@ -227,6 +271,76 @@ void AddElemenToListProduct(int deadline, int amount, int productId, int equipme
 
 
 }
+
+void AddElemenToListJsonOrder(char A[], char B[], char C[], char D[], int lenghtA, int lenghtB, int lenghtC, int lenghtD) {
+
+	int deadline = 0;
+	int amount = 0;
+	int IdProd = 0;
+	for (int i = 0; i < lenghtD; i++) {
+		deadline = 10 * deadline + (D[i] - '0');
+	}
+	for (int i = 0; i < lenghtC; i++) {
+		amount = 10 * amount + (C[i] - '0');
+	}
+	for (int i = 0; i < lenghtB; i++) {
+		IdProd = 10 * IdProd + (B[i] - '0');
+	}
+
+	//cout << B;
+	//system("pause");
+
+	AddElemenToListProduct(deadline, amount, IdProd);
+
+}
+
+void AddElemenToListJsonProduct(char A[], char B[], char C[], char D[], int lenghtA, int lenghtB, int lenghtC, int lenghtD) {
+	int productId = 0;
+	int idEq = 0, j = 0;
+	for (int i = 0; i < lenghtA; i++) {
+		productId = 10 * productId + (A[i] - '0');
+	}
+
+	int *equipmentClass = new int[10];
+	for (int i = 0; i < 10; i++) {
+		equipmentClass[i] = 0;
+	}
+
+
+
+	for (int i = 0; i < lenghtB; i++) {
+		if (B[i] == ',') {
+			//cout << idEq << endl;
+			equipmentClass[j] = idEq;
+			j++;
+			idEq = 0;
+		}
+		else {
+			idEq = 10 * idEq + (B[i] - '0');
+		}
+		
+	}
+	equipmentClass[j] = idEq;
+
+	Product *temp = new Product;
+	temp = firstElementProduct;
+
+	while (temp) {
+		if (temp->productId == productId) {
+			temp->equipmentClass = equipmentClass;
+		}
+		temp = temp->next;
+	}
+
+
+
+
+}
+
+
+
+
+
 
 void AddElemenToListPlan(int productId, int equipmentID, int timeStart, int timeFinish, int count)
 {
@@ -253,10 +367,10 @@ void AddElemenToListPlan(int productId, int equipmentID, int timeStart, int time
 
 }
 
-Equipment * searchEquipment() {
+Equipment * searchEquipment(Product * temp1) {
 	Equipment *temp = new Equipment;
-	Product *temp1 = new Product;
-	temp1 = secondElementProduct;
+	//Product *temp1 = new Product;
+	//temp1 = firstElementProduct;
 	temp = firstElementEquipment;
 	
 
@@ -272,30 +386,34 @@ Equipment * searchEquipment() {
 		temp = temp->next;
 	}
 
-
+	return temp;
 }
 
 
 
 
 
-
+////////////////////////////////////////////////////////////////
 void ShowSingleLinkedListProduct() {
 	Product *temp = new Product;
 	temp = firstElementProduct;
 
 	while (temp) {
-		cout << temp->deadline << " " << temp->amount << " " << temp->productId << " " << temp->cof ;
-		cout << endl;
+		cout << temp->deadline << " " << temp->amount << " " << temp->productId << " " << temp->cof; //" " << temp->cof ;
+		//cout << temp->deadline << " " << temp->amount << " " << temp->productId;
+		
 		for (int i = 0; i < 10; i++) {
-			cout << temp->equipmentClass[i];
+			cout << temp->equipmentClass[i] << " ";
 		}
-		cout << endl;
+		//cout << endl;
 		temp = temp->next;
+		cout << endl;
+		//system("pause");
 	}
 }
 
 void ShowSingleLinkedList(){
+	
 	Equipment *temp = new Equipment;
 	temp = firstElementEquipment;
 
@@ -318,65 +436,56 @@ void ShowSingleLinkedListPlan() {
 	}
 }
 
+void ShowSingleLinkedListJsonBlock() {
+	jsonBlock *temp = new jsonBlock;
+	temp = firstElementJsonBlock;
+
+	while (temp) {
+		cout << temp->A << " " << temp->B << " " << temp->C << temp->D << endl;
+		temp = temp->next;
+	}
+}
+
+///////////////////////////////////////////////////////////////////
 
 
 
 
 void createPlan() {
+	
 	while (secondElementProduct != NULL){
 		Equipment *temp = new Equipment;
 		Product *temp2 = new Product;
 		Product *temp3 = new Product;
 		plan *temp1 = new plan;
-		int productId = secondElementProduct->productId;
-		int amount = secondElementProduct->amount;
+
+
 		temp3 = secondElementProduct;
-		
-		temp = searchEquipment();
-		if (temp->speedPerHour < secondElementProduct->amount) {
-			AddElemenToListPlan(productId, temp->equipmentID, temp->time,3600+temp->time, temp->speedPerHour);
-			temp->time = 3600 + temp->time;
-			secondElementProduct->amount = secondElementProduct->amount - temp->speedPerHour;
-			Cof();
-			mergeSortProduct(firstElementProduct);
-			secondElementProduct = firstElementProduct->next;
-		}
-		else if (temp->speedPerHour >= secondElementProduct->amount) {
-			int timeOneProduct = 3600 / temp->speedPerHour;
-
-			/*system("pause");
-			cout << timeOneProduct * amount << " " << temp->time << "  " << temp->equipmentID;*/
-
-			AddElemenToListPlan(productId, temp->equipmentID, temp->time, timeOneProduct * amount + temp->time, amount);
-			temp->time = timeOneProduct * amount + temp->time;
-			temp2 = secondElementProduct->next;
-			firstElementProduct->next = temp2;
-			delete temp3;
-			Cof();
-			mergeSortProduct(firstElementProduct);
-			secondElementProduct = firstElementProduct->next;
-		}
+		int productId = temp3->productId;
+		int amount = temp3->amount;
 		
 		
-		//mergeSortEquipment(firstElementEquipment);
+		temp = searchEquipment(secondElementProduct);
+		
+		
+		int timeOneProduct = 3600 / temp->speedPerHour;
 
+			
+
+		AddElemenToListPlan(productId, temp->equipmentID, temp->time, timeOneProduct * amount + temp->time, amount);
+
+		temp->time = timeOneProduct * amount + temp->time;
+		secondElementProduct = secondElementProduct->next;
+		//temp2 = secondElementProduct->next;
+		//delete temp2;
 		
 
 
+		//secondElementProduct = secondElementProduct->next;
 
-
-		/*int productId = firstElementProduct->productId;
-		int equipmentID = firstElementEquipment->equipmentID;
-		int timeStart = firstElementEquipment->time;
-		int timeFinish = 
-		AddElemenToListPlan()*/
-
-
-
-
-
-
-
+		//cout << productId << " " <<  temp->equipmentID << " " <<  temp->time << " " << timeOneProduct * amount + temp->time << " " <<  amount;
+	
+		//system("pause");
 	}
 
 
@@ -439,7 +548,7 @@ void compressionStart() {
 
 
 
-
+///////////////////////////////////////////////
 void saFile() {
 	plan *temp = new plan;
 	temp = firstElementPlan;
@@ -457,8 +566,6 @@ void saFile() {
 
 	
 }
-
-
 
 void sa() {
 	plan *temp = new plan;
@@ -509,16 +616,448 @@ void sa() {
 
 }
 
-
-
-
-
-
-
-
-void main() {
+void checkDeadline() {
 	
+	Product *temp;
+	temp = firstElementProduct;
+	int count = 0, timeFin;
+	while (temp) {
+		system("pause");
+		plan *tempPlan;
+		tempPlan = firstElementPlan;
+		while (tempPlan) {
+			if (temp->productId == tempPlan->productId) {
+				count += tempPlan->count;
+				timeFin = tempPlan->timeFinish;
+			}
+			tempPlan = tempPlan->next;
+		}
+		if (count > temp->amount) {
+			if (timeFin >= temp->deadline) {
+				system("pause");
+			}
+			else {
+				cout << "NO" << temp->productId;
+			}
+
+		}
+		//cout << count;
+		//system("pause");
+
+		temp = temp->next;
+	}
+
+}
+
+////////////////////////////////////////////////////////
+
+
+
+void readJsonEquipment() {
+	int coun = 0;
+
+	
+	ifstream i_File; 
+
+	i_File.open("C:\\Users\\Kirill\\source\\repos\\CourseWork\\DataSet\\equipment.json", ios::binary); // Открыли файл
+
+	if (!i_File.is_open()) // Проверили, удалось ли открыть файл
+	{
+		cout << "Открыть файл не удалось! \n";
+		return;
+	}
+
+	int valueReading = false;
+	char *A = new char[100];
+	char *B = new char[100];
+	char *C = new char[100];
+	char *D = new char[100];
+	int lengthA = 0, lengthB = 0, lengthC = 0, lengthD = 0;
+	
+	int fieldReading = false;
+	char fieldName;
+	bool fieldNameAlreadyRead = false;
+
+	char currentChar[1];
+	currentChar[0] = 'x';
+	int blockDataLength = 0;
+
+	bool blockReading = false;
+	while (i_File) {
+		if (valueReading) {
+			if (currentChar[0] == '"') {
+				fieldNameAlreadyRead = false;
+				valueReading = false;
+			} else {
+				if (fieldName == 'A') {
+					A[lengthA] = currentChar[0];
+					lengthA++;
+				}
+				if (fieldName == 'B') {
+					B[lengthB] = currentChar[0];
+					lengthB++;
+				}
+				if (fieldName == 'C') {
+					C[lengthC] = currentChar[0];
+					lengthC++;
+				}
+				if (fieldName == 'D') {
+					D[lengthD] = currentChar[0];
+					lengthD++;
+				}
+			}
+		} else if (fieldReading) {
+			if (currentChar[0] == '"') {
+				fieldReading = false;
+				fieldNameAlreadyRead = true;
+			}
+			else {
+				fieldName = currentChar[0];
+			}
+		} else if (!blockReading && currentChar[0] == '{') {
+			blockDataLength = 0;
+			blockReading = true;
+			valueReading = false;
+			fieldReading = false;
+			fieldNameAlreadyRead = false;
+			lengthA = 0;
+			lengthB = 0;
+			lengthC = 0;
+			lengthD = 0;
+			
+		} else if (blockReading && currentChar[0] == '}') {
+			blockReading = false;
+			AddElemenToListJsonEquipment(A, B, C, D, lengthA, lengthB, lengthC, lengthD);
+			/*cout << A << endl;
+			cout << B << endl;
+			cout << C << endl;
+			cout << D << endl;
+			system("pause");*/
+		} else if (blockReading && currentChar[0] == '"') {
+			if (fieldNameAlreadyRead) {
+				valueReading = true;
+			} else {
+				fieldReading = true;
+			}
+		} else {
+			//cout << currentChar[0] << "\n";
+		}
+
+		i_File.read((char *)currentChar, sizeof(currentChar));
+
+		coun++;
+		/*if (coun == 100) {
+			break;
+		}*/
+	}
+		
+	
+	//ShowSingleLinkedListJsonBlock();
+	i_File.close(); // Закрываем файл
+
+	
+	
+	
+
+}
+
+void readJsonOrder() {
+	int coun = 0;
+
+
+	ifstream i_File;
+
+	i_File.open("C:\\Users\\Kirill\\source\\repos\\CourseWork\\DataSet\\order.json", ios::binary); // Открыли файл
+
+	if (!i_File.is_open()) // Проверили, удалось ли открыть файл
+	{
+		cout << "Открыть файл не удалось! \n";
+		return;
+	}
+
+	int valueReading = false;
+	char *A = new char[100];
+	char *B = new char[100];
+	char *C = new char[100];
+	char *D = new char[100];
+	int lengthA = 0, lengthB = 0, lengthC = 0, lengthD = 0;
+
+	int fieldReading = false;
+	char fieldName;
+	bool fieldNameAlreadyRead = false;
+
+	char currentChar[1];
+	currentChar[0] = 'x';
+	int blockDataLength = 0;
+
+	bool blockReading = false;
+	while (i_File) {
+		if (valueReading) {
+			if (currentChar[0] == '"') {
+				fieldNameAlreadyRead = false;
+				valueReading = false;
+			}
+			else {
+				if (fieldName == 'A') {
+					A[lengthA] = currentChar[0];
+					lengthA++;
+				}
+				if (fieldName == 'B') {
+					B[lengthB] = currentChar[0];
+					lengthB++;
+				}
+				if (fieldName == 'C') {
+					C[lengthC] = currentChar[0];
+					lengthC++;
+				}
+				if (fieldName == 'D') {
+					D[lengthD] = currentChar[0];
+					lengthD++;
+				}
+			}
+		}
+		else if (fieldReading) {
+			if (currentChar[0] == '"') {
+				fieldReading = false;
+				fieldNameAlreadyRead = true;
+			}
+			else {
+				fieldName = currentChar[0];
+			}
+		}
+		else if (!blockReading && currentChar[0] == '{') {
+			blockDataLength = 0;
+			blockReading = true;
+			valueReading = false;
+			fieldReading = false;
+			fieldNameAlreadyRead = false;
+			lengthA = 0;
+			lengthB = 0;
+			lengthC = 0;
+			lengthD = 0;
+
+		}
+		else if (blockReading && currentChar[0] == '}') {
+			blockReading = false;
+			AddElemenToListJsonOrder(A, B, C, D, lengthA, lengthB, lengthC, lengthD);
+			/*cout << A << endl;
+			cout << B << endl;
+			cout << C << endl;
+			cout << D << endl;
+			system("pause");*/
+		}
+		else if (blockReading && currentChar[0] == '"') {
+			if (fieldNameAlreadyRead) {
+				valueReading = true;
+			}
+			else {
+				fieldReading = true;
+			}
+		}
+		else {
+			//cout << currentChar[0] << "\n";
+		}
+
+		i_File.read((char *)currentChar, sizeof(currentChar));
+
+		coun++;
+		/*if (coun == 100) {
+			break;
+		}*/
+	}
+
+
+	//ShowSingleLinkedListJsonBlock();
+	i_File.close(); // Закрываем файл
+
+
+
+
+
+}
+
+void readJsonProduct() {
+	int coun = 0;
+
+
+	ifstream i_File;
+
+	i_File.open("C:\\Users\\Kirill\\source\\repos\\CourseWork\\DataSet\\product.json", ios::binary); // Открыли файл
+
+	if (!i_File.is_open()) // Проверили, удалось ли открыть файл
+	{
+		cout << "Открыть файл не удалось! \n";
+		return;
+	}
+
+	int valueReading = false;
+	char *A = new char[100];
+	char *B = new char[100];
+	char *C = new char[100];
+	char *D = new char[100];
+	int lengthA = 0, lengthB = 0, lengthC = 0, lengthD = 0;
+
+	int fieldReading = false;
+	char fieldName;
+	bool fieldNameAlreadyRead = false;
+
+	char currentChar[1];
+	currentChar[0] = 'x';
+	int blockDataLength = 0;
+
+	bool blockReading = false;
+	while (i_File) {
+		if (valueReading) {
+			if (currentChar[0] == '"') {
+				fieldNameAlreadyRead = false;
+				valueReading = false;
+			}
+			else {
+				if (fieldName == 'A') {
+					A[lengthA] = currentChar[0];
+					lengthA++;
+				}
+				if (fieldName == 'B') {
+					B[lengthB] = currentChar[0];
+					lengthB++;
+				}
+				if (fieldName == 'C') {
+					C[lengthC] = currentChar[0];
+					lengthC++;
+				}
+				if (fieldName == 'D') {
+					D[lengthD] = currentChar[0];
+					lengthD++;
+				}
+			}
+		}
+		else if (fieldReading) {
+			if (currentChar[0] == '"') {
+				fieldReading = false;
+				fieldNameAlreadyRead = true;
+			}
+			else {
+				fieldName = currentChar[0];
+			}
+		}
+		else if (!blockReading && currentChar[0] == '{') {
+			blockDataLength = 0;
+			blockReading = true;
+			valueReading = false;
+			fieldReading = false;
+			fieldNameAlreadyRead = false;
+			lengthA = 0;
+			lengthB = 0;
+			lengthC = 0;
+			lengthD = 0;
+
+		}
+		else if (blockReading && currentChar[0] == '}') {
+			blockReading = false;
+			AddElemenToListJsonProduct(A, B, C, D, lengthA, lengthB, lengthC, lengthD);
+			/*cout << A << endl;
+			cout << B << endl;
+			cout << C << endl;
+			cout << D << endl;
+			system("pause");*/
+		}
+		else if (blockReading && currentChar[0] == '"') {
+			if (fieldNameAlreadyRead) {
+				valueReading = true;
+			}
+			else {
+				fieldReading = true;
+			}
+		}
+		else {
+			//cout << currentChar[0] << "\n";
+		}
+
+		i_File.read((char *)currentChar, sizeof(currentChar));
+
+		coun++;
+		/*if (coun == 100) {
+			break;
+		}*/
+	}
+
+
+	//ShowSingleLinkedListJsonBlock();
+	i_File.close(); // Закрываем файл
+
+
+
+
+
+}
+
+
+
+	
+
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
 	int mas0[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	AddElemenToListProduct(1, 1, 1);
+	firstElementProduct->equipmentClass = mas0;
+	AddElemenToListEquipment(1, 0);
+
+
+
+	readJsonOrder();
+	readJsonProduct();
+	readJsonEquipment();
+	//ShowSingleLinkedListProduct();
+	
+	//Cof();
+
+	mergeSortProduct(firstElementProduct);
+	secondElementProduct = firstElementProduct->next;
+	//ShowSingleLinkedListProduct();
+	mergeSortEquipment(firstElementEquipment);
+	
+	firstElementEquipment = firstElementEquipment->next;
+	//ShowSingleLinkedList();
+	//system("pause");
+	createPlan();
+	ShowSingleLinkedListPlan();
+	//compressionStart();
+	//saFile();
+	//sa();
+	
+
+
+
+
+	//ShowSingleLinkedListProduct();
+	
+
+	
+	//ShowSingleLinkedList();
+	
+	/*int mas0[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	int mas1[] = { 1, 4, 7, 2, 0, 0, 0, 0, 0, 0 };
 	int mas2[] = { 5, 7, 2, 9, 18, 6, 0, 0, 0, 0 };
 	int mas3[] = { 1, 9, 11, 45, 0, 0, 0, 0, 0, 0 };
@@ -536,7 +1075,7 @@ void main() {
 
 
 
-	AddElemenToListEquipment(6, 1);
+	AddElemenToListEquipment(1, 0);
 	AddElemenToListEquipment(9, 4);
 	AddElemenToListEquipment(13, 7);
 	AddElemenToListEquipment(79, 2);
@@ -565,7 +1104,7 @@ void main() {
 	compressionStart();
 
 
-
+	//checkDeadline();
 
 	saFile();
 	//ShowSingleLinkedListPlan();
@@ -573,10 +1112,10 @@ void main() {
 	//ShowSingleLinkedListProduct();
 	//cout << endl;
 	//ShowSingleLinkedList();
+	*/
 
 
-
-
+	return 0;
 }
 
 
